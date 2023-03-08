34e3db21580c006fb68ea49b60bc2ca20e996ec5
Solomon Kidanu <solog0039@gmail.com>
2023-03-08 17:12:59 +0300

    mobile menu added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const myNavv = document.getElementById('myNav');
 function openNav() {
-  document.getElementById('myNav').style.width = '100%';
+  myNavv.style.width = '100%';
 }
 function closeNav() {
   document.getElementById('myNav').style.width = '0%';
 }
+const openNavv = document.getElementById('.myNav');
+const closeNavv = document.getElementById('.myNav');
+openNavv.addEventListener('click', openNav);
+closeNavv.addEventListener('click', closeNav);
